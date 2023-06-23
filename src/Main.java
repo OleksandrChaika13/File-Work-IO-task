91bd47ad0da28fda2bacd08ec3c520c4a2850e95
OleksandrChaika13 <axelrod1347@gmail.com>
2023-06-23 15:09:59 +0300

    Add options menu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner scanner;
 
     public static void main(String[] args) {
 
         System.out.println("My app for creating and reading files!");
+    }
+
+    private static int getOption() {
+
+        scanner = new Scanner(System.in);
+
+        System.out.print("""
+                Choose an option:
+                1 - Create and write a file
+                2 - Read a file
+                0 - Close the app
+                """);
+        return OptionValidator.validateOptionInput(scanner);
     }
 }
