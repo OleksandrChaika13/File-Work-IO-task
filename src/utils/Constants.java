b0c29c0aa535a48a9bc655e02801c688076e1345
OleksandrChaika13 <axelrod1347@gmail.com>
2023-06-23 14:58:14 +0300

    Add constants

---
+++ src/utils/Constants.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class Constants {
+
+    public static final String BASE_URL = "files/";
+    public final static String NO_SUCH_OPTION_MSG = "There is no such option! Try again: ";
+    public final static String NAN_NUMBER_MSG = "\"%s\" - not a number! Try again: ";
+    public final static String APP_CLOSED_MSG = "\n>> App closed.";
+}
